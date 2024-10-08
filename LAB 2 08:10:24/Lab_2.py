b0c93eb9726cab70ab74903a80f56c708b8eeaf0
lancedtaylor <lancedtaylor@icloud.com>
2024-10-08 12:48:48 +0100

    lab2 commit

---
+++ LAB 2 08:10:24/Lab_2.py
@@ -0,0 +1,14 @@
+
+my_text = "   I have an apple, you have an apple   "
+#my_text = my_text.replace("apple", "orange")
+my_text = my_text.strip()
+print(my_text)
+
+
+'''
+my_text = "Python Java C++"
+my_text = my_text.split()
+print(my_text)
+print(type(my_text))
+'''
+
